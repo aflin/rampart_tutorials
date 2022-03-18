99fe1dc2cf71fdb98d7bfb3fad73caf5e7e8570b
aflin <aflin@bigsink.com>
2022-03-17 19:54:48 -0700

    script now matches tutorial

--- geonames/import.js
+++ geonames/import.js
@@ -1,54 +1,55 @@
-rampart.globalize(rampart.utils)
-var Sql=require("rampart-sql");
+var Sql = require("rampart-sql");
 
-var sql = new Sql.init(process.scriptPath + "/web_server/data/geonames_db", true);
+// use process.scriptPath to make sure we have the
+// correct path if running from another working directory
+var sql = new Sql.init(process.scriptPath + "/geonames_db", true);
+
+// cuz no one likes writing out 'rampart.utils.printf()'
+rampart.globalize(rampart.utils);
+
+// put the create statement into its own function
+// since we are doing this in stages
 
 function create_table() {
 
-    sql.one("drop table geonames");
-    // will get a non fatal error if table doesn't exist
-    if(sql.errMsg == '') {
-        printf("dropped old table for rebuild\n");
-    }
-
     sql.exec("create table geonames (" +
-        "id           counter, "       + 
-        "country_code char(2), "       + 
-        "postal_code  varchar(8), "    + 
-        "place_name   varchar(16), "   + 
-        "admin_name1  varchar(16), "   + 
-        "admin_code1  varchar(8), "    + 
-        "admin_name2  varchar(16), "   + 
-        "admin_code2  varchar(8), "    + 
-        "admin_name3  varchar(16), "   + 
-        "admin_code3  varchar(8), "    + 
-        "latitude     double, "        + 
-        "longitude    double, "        + 
-        "geocode      long, "          + 
-        "accuracy     int           );" 
+        "id           counter, "       +
+        "country_code char(2), "       +
+        "postal_code  varchar(8), "    +
+        "place_name   varchar(16), "   +
+        "admin_name1  varchar(16), "   +
+        "admin_code1  varchar(8), "    +
+        "admin_name2  varchar(16), "   +
+        "admin_code2  varchar(8), "    +
+        "admin_name3  varchar(16), "   +
+        "admin_code3  varchar(8), "    +
+        "latitude     double, "        +
+        "longitude    double, "        +
+        "geocode      long, "          +
+        "accuracy     int           );"
     );
+
 }
 
-var total=-1;
+
+var step = 100; //set in importCsvFile(), only report every 100th row
+var total = -1; //we won't know the total until we finish the first pass
 
 /* a single function to monitor the import for both pre-processing (progressFunc)
    and import (callback function supplied to sql.importCsvFile as a paramater)   */
 function monitor_import(count, stg) {
     var stage = "Import";
 
-    if (count==0)
-        return;
-
-    if(count==100)
+    if(count==0)
         printf("\n");
 
     if(stg!==undefined) { // progressfunc
         stage=stg;
     }
 
-    if(stg === 0)
+    if(stg === 0) //differentiate between 0 and undefined
     {
-        total=count;
+        total=count; //update our total in the first stage.
         printf("Stage: %s, Count: %d       \r", stage, count);
     } else {
         printf("Stage: %s, Count: %d of %d      \r", stage, count, total);
@@ -58,32 +59,31 @@ function monitor_import(count, stg) {
 }
 
 function import_data() {
-    var res = sql.importCsvFile(
+    total = sql.importCsvFile(
         "allCountries.txt",  //file to import
         {
-            tableName:    "geonames",
-            callbackStep: 100, //callback run every 100th row
-            progressStep: 100, //progressfunc run every 100th row for each stage
-            delimiter:    '\t',
+            normalize:       false,
             singleQuoteNest: false,
-            normalize: false, //postal code appear as numbers and strings - numbers will be converted to string on import
-                              //if this was set true, the strings would be lost, since a majority are numbers.
-            progressFunc: monitor_import //progress function while processing csv
+            delimiter:       '\t',
+            hasHeaderRow:    false,
+            tableName:       "geonames",
+            callbackStep:    step, //callback run every 100th row
+            progressStep:    step, //progressfunc run every 100th row for each stage
+            progressFunc:    monitor_import //progress function while processing csv 
         },
         /* numbers are column-in positions (-1 means leave blank, or add counter if field type is 'counter')
            position in array is column-out positions  */
         [-1,0,1,2,3,4,5,6,7,8,9,10,-1,11],
         monitor_import //callback function upon actual import
     );
-    printf("\nDone\n");
-    total=res;
+    printf('\n');//end with a newline
 }
 
 function make_geocode() {
     printf("Computing geocode column:\n");
 
-    sql.exec("update geonames set "+
-             "geocode = latlon2geocode(latitude, longitude);",
+    sql.exec("update geonames set geocode = latlon2geocode(latitude, longitude);",
+             //monitor our progress with a callback
              function(row,i) {
                  if(! (i%100) ) {
                      printf("%d of %d    \r", i, total);
@@ -91,6 +91,7 @@ function make_geocode() {
                  }
              }
     );
+    printf('\n');//end with a newline
 }
 
 function make_geocode_index() {
@@ -98,7 +99,12 @@ function make_geocode_index() {
     sql.exec("create index geonames_geocode_x on geonames(geocode) WITH INDEXMETER 'on';");
 }
 
-function make_text_index(){
+function make_id_index(){
+    printf("creating index on id\n");
+    sql.exec("create index geonames_id_x on geonames(id) WITH INDEXMETER 'on';");
+}
+
+function make_text_index() {
     printf("creating indexes on location names\n");
 
     // noiselist as detailed at https://rampart.dev/docs/sql-set.html#noiselist
@@ -111,18 +117,12 @@ function make_text_index(){
         " WITH WORDEXPRESSIONS ('[\\alnum\\x80-\\xFF]{2,99}') INDEXMETER 'on';");
 }
 
-function make_id_index(){
-    printf("creating index on id\n");
-
-    sql.exec("create index geonames_id_x on geonames(id) WITH INDEXMETER 'on';");
-
-}
-
 create_table();
 import_data();
 make_geocode();
 make_geocode_index();
-make_text_index();
 make_id_index();
+make_text_index();
 
 printf("All Done\n");
+
